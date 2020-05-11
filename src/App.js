a2d1d4b4c60b423e0f946ee6211d7166c2259b1f
Chirag <chirag.gohil@adrosonic.com>
2020-05-11 18:10:44 +0530

    Added demo translation code

--- src/App.js
+++ src/App.js
@@ -1,27 +1,35 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Home from './containers/home/Home';
-import { useTranslation } from "react-i18next";
+import { useTranslation } from 'react-i18next';
+import i18next from 'i18next';
 
 function App() {
- 
+  const { t } = useTranslation();
 
-function handleclick(lang){
-i18n.changeLanguage(lang);
-}
-
-
-
+  function handleClick(lang) {
+    i18next.changeLanguage(lang)
+  }
 
   return (
     <div className="App">
-      <nav style={{ width: "100%", padding: "2em", background: "grey" }}>
-        <button onClick={() => handleclick("en")}>English</button>
-        <button onClick={() => handleclick("hi")}>Hindi</button>
+      <nav style={{ width: '100%', padding: '2rem 0', backgroundColor: 'gray' }}>
+        <button onClick={() => handleClick('en')} >
+          English
+          </button>
+        <button onClick={() => handleClick('ko')} >
+          Korean
+          </button>
+        <button onClick={() => handleClick('chi')} >
+          Chinese
+         </button>
       </nav>
-      <p>{t("my translated text")}</p>
-      <Home></Home>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <h3>{t('Thanks.1')}</h3>  <h3>{t('Why.1')}</h3>
+        </p>
+      </header>
     </div>
   );
 }

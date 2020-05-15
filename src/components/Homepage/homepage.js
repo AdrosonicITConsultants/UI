022b9bdc552d5484263a0750f379617b8ed401e7
Chirag <chirag.gohil@adrosonic.com>
2020-05-15 18:29:36 +0530

    made changes for buyer and artisian login page

--- src/components/Homepage/homepage.js
+++ src/components/Homepage/homepage.js
@@ -3,28 +3,71 @@ import { Row, Col , Container} from 'reactstrap';
 import "./homepage.css"
 import logo from "../../assets/logo.png";
 import Roleselect from "./roleselect"
+import Buyerpass from "../buyer/buyerpass";
+import Buyeruser from "../buyer/buyeruser";
+import Artistlogin from "../artist/artistlogin"
 
 export default class HomePage extends Component {
-    render() {
+                 constructor(props) {
+                   super(props);
 
-        return(
-            <React.Fragment> 
-                <div className ="homeimg"  >
-                    <Container>
-                    <Row noGutters={true} className="mt-5">
-                        <Col xs={{size:'12'}} sm={{size:'5'}} md={{size:'6'}} md={{size:'7'}}>
-                            
-                        </Col>
-                        <Col md={{size:'12'}} sm={{size:'7'}} md={{size:'6'}} md={{size:'5'}}>
-                            <Roleselect/>
-                             
-                        </Col>
-                    </Row>
+                     this.state = {
+                       userpage : 0
+                     };
+                   this.handler = this.handler.bind(this);
+                  
+                 }
 
-                    </Container>
-                </div>
-            </React.Fragment> 
-        )
-    }
+                 renderSection(num){
+                     switch (num) {
+                       case 0:
+                         return <Buyeruser handler={this.handler} />;
+                         break;
+                       case 1:
+                         return <Buyerpass handler={this.handler} />;
+                         break;
+                       case 2:
+                         return <Artistlogin handler={this.handler} />;
+                         break;
+                       default:
+                         break;
+                     }
+                 }
+                 
+                 handler(num) {              
+                   this.setState({ userpage : num }, () => {
+                     console.log(this.state.userpage);
+                   });                    
+                 }
 
-}
+                 render() {
+                   return (
+                     <React.Fragment>
+                       <div className="homeimg">
+                         <Container>
+                           <Row noGutters={true} className="mt-5">
+                             <Col
+                               xs={{ size: "12" }}
+                               sm={{ size: "5" }}
+                               md={{ size: "6" }}
+                               md={{ size: "7" }}
+                             ></Col>
+                             <Col
+                               md={{ size: "12" }}
+                               sm={{ size: "7" }}
+                               md={{ size: "6" }}
+                               md={{ size: "5" }}
+                             >
+                               
+                               {this.renderSection(this.state.userpage)}
+                               {/* <Buyerpass handler={this.handler} /> */}
+                               {/* <Buyeruser handler={this.handler} /> */}
+                               {/* <Artistlogin handler={this.handler} /> */}
+                             </Col>
+                           </Row>
+                         </Container>
+                       </div>
+                     </React.Fragment>
+                   );
+                 }
+               }
---
+++ src/components/buyer/buyeruser.js
@@ -0,0 +1,131 @@
+import React, { Component } from "react";
+import { Row, Col, Container } from "reactstrap";
+import "../Homepage/homepage.css";
+import logo from "../../assets/logo.svg";
+
+export default class buyeruser extends Component {
+  constructor() {
+    super();
+    this.state = {
+      userName: "",   
+      showValidation: false,
+      showUserName: true,
+    };
+  }
+
+  operation() {
+    debugger;
+    if (this.state.userName == "") {
+      this.setState({
+        showValidation: !this.state.showValidation,
+      });
+    } else {
+     this.props.handler(1);
+    }
+  } 
+
+  handleChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+    this.setState({
+      showValidation: false,
+    });
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <div className="demo" noGutters={true}>
+          <br></br>
+          <Row noGutters={true} className="">
+            <span className="col-sm-2 glyphicon glyphicon-arrow-left"></span>
+            <h2 className="col-sm-8">Login</h2>
+            <img src={logo} className="col-sm-2" alt="TataTrusts logo"></img>
+          </Row>
+
+          <br />
+          <Row noGutters={true}>
+            <span className="col-sm-4"></span>
+            <img src={logo} className="col-sm-4" alt="TataTrusts logo"></img>
+            <span className="col-sm-4"></span>
+          </Row>
+
+          <br />         
+            <div>
+              <Row noGutters={true}>
+                <span className="col-sm-1"></span>
+                <span className="col-sm-10">
+                  Login using your mobile number or email id
+                </span>
+              </Row>
+              <Row noGutters={true}>
+                <div className="col-sm-1"></div>
+                <div className="form-group col-sm-10 col-xs-6">
+                  <label className="control-label"></label>
+                  <div className="inner-addon left-addon">
+                    <i className="glyphicon glyphicon-user"></i>
+                    <input
+                      type="text"
+                      id="userName"
+                      className="form-control BuyerLogin"
+                      placeholder="Username"
+                      name="userName"
+                      onChange={(e) => this.handleChange(e)}
+                    />
+                    {this.state.showValidation ? (
+                      <span className="bg-danger">please enter username</span>
+                    ) : null}
+                  </div>
+                </div>
+              </Row>
+            </div>        
+          <br />
+            <Row noGutters={true}>
+              <div className="col-sm-12 text-center">
+                <button
+                  style={{
+                    background: "#000000",
+                    color: "white",
+                    borderRadius: "2em",
+                    width: "8em",
+                    height: "3em",
+                  }}
+                  onClick={() => this.operation()}
+                >
+                  Next
+                </button>
+              </div>
+            </Row>         
+          <br />
+          {/* <Row noGutters={true}>
+                <div className="col-sm-12 text-center">
+                  <h2>Or</h2>
+                </div>
+              </Row>
+
+              <br />
+
+              <Row noGutters={true}>
+                <div className="col-sm-12 text-center">
+                  <h2>google login</h2>
+                </div>
+              </Row> */}
+          <hr className="hrline"></hr>
+          <Row noGutters={true}>
+            <div className="col-sm-12 text-center">
+              <div>
+                new user <a href="./">click here</a> to register.
+              </div>
+            </div>
+          </Row>
+
+          <Row noGutters={true}>
+            <span className="col-sm-3 text-center">Help</span>
+            <span className="col-sm-5"></span>
+            <span className="col-sm-4 text-center">Privacy policy</span>
+          </Row>
+          <br></br>
+        </div>
+      </React.Fragment>
+    );
+  }
+}

3fb95e4ffacb918962b1ae579285c1c3c6f29c55
Chirag <chirag.gohil@adrosonic.com>
2020-05-27 15:33:26 +0530

    made changes fot navbar

---
+++ src/components/landingpage/landingpage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Navbar from "../navbar/navbar"
+
+export default class landingpage extends Component {
+    render() {
+        return (
+            <div>
+                <Navbar></Navbar>
+            </div>
+        )
+    }
+}

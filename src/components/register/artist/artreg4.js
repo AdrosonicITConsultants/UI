72edec5a7811aeb45d63a81f0069cb7b0febbdec
piyush-khandelwal-adrosonic <58552322+piyush-khandelwal-adrosonic@users.noreply.github.com>
2020-05-29 10:32:18 +0530

    tt

--- src/components/register/artist/artreg4.js
+++ src/components/register/artist/artreg4.js
@@ -154,7 +154,7 @@ export default class artreg4 extends Component {
                   <span className="col-xs-10">
                     {/* <img src={logos.locklogo}   
                     className="locklogo1 glyphicon mr-5"></img> */}
-                    WEAVER ID : {this.props.weaverid}
+                    <span className = "fontplay">ARTISIAN ID : </span>{this.props.weaverid}
                   </span>
                 </Row>
                 <Row noGutters={true}>

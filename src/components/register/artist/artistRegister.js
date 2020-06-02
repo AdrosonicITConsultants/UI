818c7a16c7d88b3fe267fad0eaca86db1a77a7a9
piyush-khandelwal-adrosonic <58552322+piyush-khandelwal-adrosonic@users.noreply.github.com>
2020-06-02 18:31:59 +0530

    changes

--- src/components/register/artist/artistRegister.js
+++ src/components/register/artist/artistRegister.js
@@ -20,7 +20,7 @@ export default class artistRegister extends Component {
                    super(props);
 
                    this.state = {
-                     userpage: 1,
+                     userpage: 4,
                      weaverid: "",
                      emailid: "",
                      password: "",

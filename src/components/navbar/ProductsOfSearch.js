447915b74eb50047517437009ed9885305601bc6
your <chirag.gohil@adrosonic.com>
2020-07-14 15:30:38 +0530

    Revert "tt"
    This reverts commit 934abaedc39dcc782901f928dc07a617056b72c1.

--- src/components/navbar/ProductsOfSearch.js
+++ src/components/navbar/ProductsOfSearch.js
@@ -16,20 +16,20 @@ import TTCEapi from '../../services/API/TTCEapi';
 export class ProductsOfSearch extends Component {
     constructor(props) {
         super(props);
-        console.log(this.props);
+
         this.state = {
-            
             proddata : this.props.productData,
             imagename :this.props.productData.images.split(",")[0],
             isfavHovered :false,
             imageurl : logos.dupatta,
             imageUrl : TTCEapi.ImageUrl +"Product/",
-            isAddedtoWishlist: this.props.productIdsInWishlist,
+            isAddedtoWishlist:false,
+            productIdsInWishlist:this.props.productIdsInWishlist,
             addToWishlist:null,
             deleteProductsInWishlist:[]
         };
         this.handleAddtoWishlist = this.handleAddtoWishlist.bind(this);
-    //   console.log(this.props);
+      console.log(this.props);
     }
     handleAddtoWishlist(id){
       TTCEapi.addToWishlist(id).then((response)=>{
@@ -56,10 +56,28 @@ export class ProductsOfSearch extends Component {
           if(response.data.data=="Successfull"){
             this.setState({isAddedtoWishlist:false})
           }
+        
+          
+    
     });
     }
     componentDidMount(){
-        // console.log(this.state);
+      if(this.state.productIdsInWishlist){
+     if(this.state.productIdsInWishlist.indexOf(this.state.proddata.id)!=-1)
+     {
+       this.setState({
+         isAddedtoWishlist:true
+       })
+     }
+    }
+     TTCEapi.getProductsInWishlist().then((response)=>{
+      this.setState({getProductsInWishlist : response.data.data},()=>{
+          console.log(this.state.getProductsInWishlist);
+         
+      });
+  }); 
+
+  console.log(this.state.proddata.images.split(",")[0])
     }
 
 

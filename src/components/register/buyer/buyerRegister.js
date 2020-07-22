f013dffdad5412c29ca702a7b9e4eae817c00fbe
piyush-khandelwal-adrosonic <58552322+piyush-khandelwal-adrosonic@users.noreply.github.com>
2020-07-22 14:18:22 +0530

    tt

--- src/components/register/buyer/buyerRegister.js
+++ src/components/register/buyer/buyerRegister.js
@@ -22,7 +22,7 @@ export default class buyerRegister extends Component {
                    super(props);
 
                    this.state = {
-                     userpage: 5,
+                     userpage: 0,
                      emailid: "",
                      password: "",
                      firstname: "",
